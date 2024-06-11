b8591474e7d7d00b4a5e140cffa943991afca8fc
Veronika Timakova <tymnik@ukr.net>
2024-06-11 23:23:26 +0300

    final fixes

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,18 +1,18 @@
 import img1 from '../img/cards/img1.webp'
-import img2 from '../img/cards/img1.webp';
-import img3 from '../img/cards/img1.webp';
-import img4 from '../img/cards/img1.webp';
-import img5 from '../img/cards/img1.webp';
-import img6 from '../img/cards/img1.webp';
-import img7 from '../img/cards/img1.webp';
-import img8 from '../img/cards/img1.webp';
-import img9 from '../img/cards/img1.webp';
-import img10 from '../img/cards/img1.webp';
-import img11 from '../img/cards/img1.webp';
-import img12 from '../img/cards/img1.webp';
-import img13 from '../img/cards/img1.webp';
-import img14 from '../img/cards/img1.webp';
-import img15 from '../img/cards/img1.webp';
+import img2 from '../img/cards/img2.webp';
+import img3 from '../img/cards/img3.webp';
+import img4 from '../img/cards/img4.webp';
+import img5 from '../img/cards/img5.webp';
+import img6 from '../img/cards/img6.webp';
+import img7 from '../img/cards/img7.webp';
+import img8 from '../img/cards/img8.webp';
+import img9 from '../img/cards/img9.webp';
+import img10 from '../img/cards/img10.webp';
+import img11 from '../img/cards/img11.webp';
+import img12 from '../img/cards/img12.webp';
+import img13 from '../img/cards/img13.webp';
+import img14 from '../img/cards/img14.webp';
+import img15 from '../img/cards/img15.webp';
 
 export const cards = [
   {
